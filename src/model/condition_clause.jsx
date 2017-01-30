21dfef9d5bef8f6129d69e0d6b2b8df1c4bfb568
Valerii Martseniuk <valeriy.mars@gmail.com>
2017-01-30 13:41:49 -0800

    woops

--- src/model/condition_clause.jsx
+++
@@ -1,5 +0,0 @@
-export default class ConditionClause {
-    name = "";
-    value = "";
-    operator = "";
-};
--- src/model/query.jsx
+++
@@ -1,9 +0,0 @@
-import Clause from './condition_clause.jsx';
-
-export default class Query {
-    table_name = '';
-    start_time = '';
-    end_time = '';
-    select_fields = [];
-    where_clause = [[new Clause()]];
-};
